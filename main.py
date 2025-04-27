ee41417b425a758fbe2645a7a83653ebaae0c194
gimz12 <kumalillankoon12@gmail.com>
2025-04-27 13:35:24 +0530

    Updated Game to take User Input to take Size Of the Board

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-# main.py
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QInputDialog
 from ui.main_menu import MainMenu
 from db.database import create_table
-from ui.comparison_window import ComparisonWindow  # import here
+from ui.comparison_window import ComparisonWindow  
 
-# **1) Store it at module–level so it won't be GC'd**
 comparison_window = None
 
 def start_game():
     from ui.game_board import GameBoard
-    board = GameBoard(lambda: None, lambda: None)
-    board.show()
+    
+    # Ask the user for the board size
+    board_size, ok = QInputDialog.getInt(None, "Select Board Size", "Enter board size:", 8, 4, 12, 1)
+    if ok:
+        board = GameBoard(board_size=board_size)
+        board.show()
 
 def compare_algorithms():
     global comparison_window
-    board_size = 6  # You can change board size if you want
+    board_size = 6  # Board size 
     comparison_window = ComparisonWindow(board_size=board_size)
     comparison_window.show()
-
 
 app = QApplication(sys.argv)
 create_table()
